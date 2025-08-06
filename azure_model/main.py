51f19c64ed713dc8a8d43ee2e1655d00a5ba6f47
james <james@obiguard.com>
2025-08-07 00:25:57 +0800

    Use Obiguard with Azure model and latest crewai.

--- azure_model/main.py
+++ azure_model/main.py
@@ -1,42 +1,40 @@
-import sys
 from crewai import Agent, Task
-import os
+from crewai import Crew, Process, LLM
 from dotenv import load_dotenv
-from crewai import Crew, Process
-from langchain_openai import AzureChatOpenAI
+from obiguard import Obiguard, OBIGUARD_GATEWAY_URL
 
 load_dotenv()
 
-default_llm = AzureChatOpenAI(
-    openai_api_version=os.environ.get("AZURE_OPENAI_VERSION", "2023-07-01-preview"),
-    azure_deployment=os.environ.get("AZURE_OPENAI_DEPLOYMENT", "gpt35"),
-    azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT", "https://<your-endpoint>.openai.azure.com/"),
-    api_key=os.environ.get("AZURE_OPENAI_KEY")
-)
+obiguard = Obiguard(provider='openai')
 
+default_llm = LLM(
+    model="openai/Qwen/Qwen2.5-32B-Instruct",
+    base_url=OBIGUARD_GATEWAY_URL,
+    api_key='N/A',
+    extra_headers=obiguard.copy_headers()
+)
 
 # Create a researcher agent
 researcher = Agent(
-  role='Senior Researcher',
-  goal='Discover groundbreaking technologies',
-  verbose=True,
-  llm=default_llm,
-  backstory='A curious mind fascinated by cutting-edge innovation and the potential to change the world, you know everything about tech.'
+    role='Senior Researcher',
+    goal='Discover groundbreaking technologies',
+    verbose=True,
+    llm=default_llm,
+    backstory='A curious mind fascinated by cutting-edge innovation and the potential to change the world, you know everything about tech.'
 )
 
 # Task for the researcher
 research_task = Task(
-  description='Identify the next big trend in AI',
-  expected_output='5 paragraphs on the next big AI trend',
-  agent=researcher  # Assigning the task to the researcher
+    description='Identify the next big trend in AI',
+    expected_output='5 paragraphs on the next big AI trend',
+    agent=researcher  # Assigning the task to the researcher
 )
-
 
 # Instantiate your crew
 tech_crew = Crew(
-  agents=[researcher],
-  tasks=[research_task],
-  process=Process.sequential  # Tasks will be executed one after the other
+    agents=[researcher],
+    tasks=[research_task],
+    process=Process.sequential  # Tasks will be executed one after the other
 )
 
 # Begin the task execution
